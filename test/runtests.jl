59f5eb6da8ecb24520ce8e54935b4271ad358b30
Martin Johansson <stormaktsporter@gmail.com>
2019-10-06 13:20:06 +0200

    Additional floating point test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,8 +2,9 @@ using GitTest
 using Test
 
 @testset "GitTest.jl" begin
-    # 2x + y
+    # 2x + 3y
     @test f(2,1) == 7
     @test f(4,3) == 17
     @test f(2.0,3) ≈ 17.0
+    @test f(3.0,-2) ≈ 0.0
 end
