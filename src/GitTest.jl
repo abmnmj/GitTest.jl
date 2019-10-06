54718fd84bad918a30034c4f84cb8aed56e015ef
Martin Johansson <stormaktsporter@gmail.com>
2019-10-06 16:02:17 +0200

    Add function derivatives

--- src/GitTest.jl
+++ src/GitTest.jl
@@ -1,7 +1,9 @@
 module GitTest
 
+using ForwardDiff
+
 include("extra_file01.jl")
 
-export f, f2
+export f, f2, dfdx
 
 end # module
--- src/extra_file01.jl
+++ src/extra_file01.jl
@@ -1,2 +1,3 @@
 f(x,y) = 2*x + 3y
 f2(x,y) = 3*x + 2*y
+dfdx(x,y) = ForwardDiff.derivative(x->f(x,y),x)
