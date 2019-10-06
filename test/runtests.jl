90688cdc6538f87ae1ad0b177859fab912c91eef
Martin Johansson <stormaktsporter@gmail.com>
2019-10-06 14:03:32 +0200

    Additional testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,9 @@ using Test
 @testset "GitTest.jl" begin
     # 2x + 3y
     @test f(2,1) == 7
+    @test f(2,1) == 7
     @test f(4,3) == 17
     @test f(2.0,3) ≈ 13.0
     @test f(3.0,-2) ≈ 0.0
+    @test f(42.0,-21) ≈ 21.0
 end
